ebb05a09bf30818b3f363bc4cfc73558cbbc7af1
balys <balysk@gmail.com>
2014-04-09 18:17:29 -0400

    Added users cookbook and databag

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name             'my_cookbook'
+maintainer       'copyright-entity'
+maintainer_email 'balys@balys.net'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "users"
